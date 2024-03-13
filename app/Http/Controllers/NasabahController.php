178697b1478eb86a9ff40404c36a25451dec5c7e
Asrofil Nadib <asrofilnadibs28@gmail.com>
2024-03-13 15:29:19 +0700

    configuration status

--- app/Http/Controllers/NasabahController.php
+++ app/Http/Controllers/NasabahController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Nasabah;
 use Illuminate\Http\Request;
 
 class NasabahController extends Controller
 {
-    //
+    public $nasabah;
+    public function index()
+    {
+        $nasabah = Nasabah::whereIn('StatusEksekusiTIF', ['Sesuai', 'Modifikasi'])
+            ->latest()
+            ->limit(20)
+            ->get();
+
+        $this->nasabah = $this->formatData($nasabah);
+//        dd($this->nasabah);
+        return view('home', [
+            'status' => $this->nasabah,
+            'sumNasabah' => Nasabah::count()
+        ]);
+    }
+
+    public function formatData($nasabah)
+    {
+        $sesuai = 0;
+        $modifikasi = 0;
+
+        foreach ($nasabah as $klien) {
+            if ($klien->StatusEksekusiTIF == 'Sesuai') {
+                $sesuai++;
+            } elseif ($klien->StatusEksekusiTIF == 'Modifikasi') {
+                $modifikasi++;
+            }
+        }
+
+        $data = [
+            'label' => ['Sesuai', 'Modifikasi'],
+            'data' => [$sesuai, $modifikasi],
+        ];
+
+        return json_encode($data);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\NasabahController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/lab', [NasabahController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
