458e8c8e26d12ad9afa13024e1269447cd4cfdde
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2024-03-14 08:53:00 +0700

    Update Tabel nasabah

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -188,75 +188,46 @@
           <div class="col-12">
             <div class="card recent-sales overflow-auto">
 
-              <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-
-                  <li><a class="dropdown-item" href="#">Today</a></li>
-                  <li><a class="dropdown-item" href="#">This Month</a></li>
-                  <li><a class="dropdown-item" href="#">This Year</a></li>
-                </ul>
-              </div>
-
               <div class="card-body">
-                <h5 class="card-title">Recent Sales <span>| Today</span></h5>
+                <h5 class="card-title">Tabel Nasabah</h5>
 
-                <table class="table table-borderless datatable">
-                  <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Customer</th>
-                    <th scope="col">Product</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Status</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                    <th scope="row"><a href="#">#2457</a></th>
-                    <td>Brandon Jacob</td>
-                    <td><a href="#" class="text-primary">At praesentium minu</a></td>
-                    <td>$64</td>
-                    <td><span class="badge bg-success">Approved</span></td>
-                  </tr>
+                <table class="table table-bordered datatable">
+                <thead>
+                <tr>
+                  <th>ID Nasabah</th>
+                  <th>ID Loan</th>
+                  <th>Siklus</th>
+                  <th>Tanggal Pencairan</th>
+                  <th>Nama File</th>
+                  <th>Tanggal Mulai GP</th>
+                  <th>Tanggal Berakhir GP</th>
+                  <th>Status Eksekusi TIF</th>
+                  <th>Tanggal Eksekusi</th>
+                  <th>Tanggal Mulai GP Penyesuaian</th>
+                  <th>Tanggal Berakhir Penyesuaian</th>
+                  <th>Status Penyesuaian</th>
+                </tr>
+                </thead>
+                <tbody>
                   <tr>
                     <th scope="row"><a href="#">#2147</a></th>
                     <td>Bridie Kessler</td>
                     <td><a href="#" class="text-primary">Blanditiis dolor omnis similique</a></td>
                     <td>$47</td>
-                    <td><span class="badge bg-warning">Pending</span></td>
+                    <td>Status Eksekusi TIF</td>
+                    <td>Tanggal Mulai GP</td>
+                    <td>Tanggal Berakhir GP</td>
+                    <td><span class="badge bg-primary">Sesuai</span></td>
+                    <td>Tanggal Eksekusi</td>
+                    <td>Tanggal Mulai GP Penyesuaian</td>
+                    <td>Tanggal Berakhir Penyesuaian</td>
+                    <td>Status Penyesuaian</td>
                   </tr>
-                  <tr>
-                    <th scope="row"><a href="#">#2049</a></th>
-                    <td>Ashleigh Langosh</td>
-                    <td><a href="#" class="text-primary">At recusandae consectetur</a></td>
-                    <td>$147</td>
-                    <td><span class="badge bg-success">Approved</span></td>
-                  </tr>
-                  <tr>
-                    <th scope="row"><a href="#">#2644</a></th>
-                    <td>Angus Grady</td>
-                    <td><a href="#" class="text-primar">Ut voluptatem id earum et</a></td>
-                    <td>$67</td>
-                    <td><span class="badge bg-danger">Rejected</span></td>
-                  </tr>
-                  <tr>
-                    <th scope="row"><a href="#">#2644</a></th>
-                    <td>Raheem Lehner</td>
-                    <td><a href="#" class="text-primary">Sunt similique distinctio</a></td>
-                    <td>$165</td>
-                    <td><span class="badge bg-success">Approved</span></td>
-                  </tr>
-                  </tbody>
+                </tbody>
                 </table>
-
               </div>
-
             </div>
-          </div><!-- End Recent Sales -->
+          </div><!-- End Table -->
 
           <!-- Top Selling -->
           <div class="col-12">
