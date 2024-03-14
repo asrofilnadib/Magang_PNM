4fccf43138cd984c12e5ad7382ce42bad8fc943b
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2024-03-14 11:05:31 +0700

    Nambahin Kolum Buat Tabel

--- resources/views/tables.blade.php
+++ resources/views/tables.blade.php
@@ -17,26 +17,38 @@
               <!-- Table with hoverable rows -->
               <div class="responsive">
                 <table class="table table-hover display" id="hoverTable">
-                  <thead>
+                <thead>
+                <tr>
+                  <th>ID Nasabah</th>
+                  <th>ID Loan</th>
+                  <th>Siklus</th>
+                  <th>Tanggal Pencairan</th>
+                  <th>Nama File</th>
+                  <th>Tanggal Mulai GP</th>
+                  <th>Tanggal Berakhir GP</th>
+                  <th>Status Eksekusi TIF</th>
+                  <th>Tanggal Eksekusi</th>
+                  <th>Tanggal Mulai GP Penyesuaian</th>
+                  <th>Tanggal Berakhir Penyesuaian</th>
+                  <th>Status Penyesuaian</th>
+                </tr>
+                </thead>
+                <tbody>
                   <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Position</th>
-                    <th scope="col">Age</th>
-                    <th scope="col">Start Date</th>
+                    <th scope="row"><a href="#">#2147</a></th>
+                    <td>Bridie Kessler</td>
+                    <td><a href="#" class="text-primary">Blanditiis dolor omnis similique</a></td>
+                    <td>$47</td>
+                    <td>Status Eksekusi TIF</td>
+                    <td>Tanggal Mulai GP</td>
+                    <td>Tanggal Berakhir GP</td>
+                    <td><span class="badge bg-primary">Sesuai</span></td>
+                    <td>Tanggal Eksekusi</td>
+                    <td>Tanggal Mulai GP Penyesuaian</td>
+                    <td>Tanggal Berakhir Penyesuaian</td>
+                    <td><span class="badge bg-primary">Tidak Ada Jadwal</span></td>
                   </tr>
-                  </thead>
-                  <tbody>
-                  @foreach($nasabah as $klien)
-                    <tr>
-                      <th scope="row">{{ $klien->id }}</th>
-                      <td>{{ $klien->NamaFile }}</td>
-                      <td>Designer</td>
-                      <td>28</td>
-                      <td>2016-05-25</td>
-                    </tr>
-                  @endforeach
-                  </tbody>
+                </tbody>
                 </table>
               </div>
               <!-- End Table with hoverable rows -->
@@ -44,37 +56,50 @@
             </div>
           </div>
 
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Small tables</h5>
-              <!-- Small tables -->
-              <div class="responsive">
+      <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Small tables</h5>
+            <!-- Small tables -->
+            <div class="responsive">
                 <table class="table table-sm display" id="smTable">
-                  <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Position</th>
-                    <th scope="col">Age</th>
-                    <th scope="col">Start Date</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  @foreach($dokumen as $doc)
-                    <tr>
-                      <th scope="row">{{ $doc->Id }}</th>
-                      <td>Brandon Jacob</td>
-                      <td>Designer</td>
-                      <td>28</td>
-                      <td>2016-05-25</td>
-                    </tr>
-                  @endforeach
-                  </tbody>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nama File</th>
+                            <th>Tanggal Terima</th>
+                            <th>Divisi Asal</th>
+                            <th>No.Memo Asal</th>
+                            <th>Perihal Memo Asal</th>
+                            <th>Tanggal Kirim</th>
+                            <th>No.Memo OBS</th>
+                            <th>Perihal Memo OBS</th>
+                            <th>No.Tiket</th>
+                            <th>Status Tiket</th>
+                            <th>Jenis GP</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><a href="#">#2147</a></td>
+                            <td>Bridie Kessler</td>
+                            <td>ja</td>
+                            <td>$47</td>
+                            <td>Status Eksekusi TIF</td>
+                            <td>Tanggal Mulai GP</td>
+                            <td>Tanggal Berakhir GP</td>
+                            <td><span class="badge bg-primary">Sesuai</span></td>
+                            <td>Tanggal Eksekusi</td>
+                            <td>Tanggal Mulai GP Penyesuaian</td>
+                            <td><span class="badge bg-primary">Tidak Ada Jadwal</span></td>
+                            <td>Tanggal Berakhir Penyesuaian</td>
+                        </tr>
+                    </tbody>
                 </table>
-              </div>
-              <!-- End small tables -->
             </div>
-          </div>
+            <!-- End small tables -->
+        </div>
+    </div>
+
 
         </div>
       </div>
