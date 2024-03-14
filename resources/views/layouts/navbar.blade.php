acc1c10c1b5c17d9834be34b0f1850d3d5302587
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2024-03-14 10:05:31 +0700

    Benerin Home

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -71,7 +71,7 @@
   <ul class="sidebar-nav" id="sidebar-nav">
 
     <li class="nav-item">
-      <a class="nav-link " href="index.html">
+      <a class="nav-link " href="lab">
         <i class="bi bi-grid"></i>
         <span>Dashboard</span>
       </a>
