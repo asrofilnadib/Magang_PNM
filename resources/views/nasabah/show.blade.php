ff4eddf5a83a2bce27ff26193edd3cd49131c082
najmulazka <najmulazka225@gmail.com>
2024-03-15 13:42:02 +0700

    show detail nasabah in view

--- resources/views/nasabah/show.blade.php
+++ resources/views/nasabah/show.blade.php
@@ -4,7 +4,7 @@
   <main id="main" class="main">
 
     <div class="pagetitle mb-3">
-      <h1>Form Elements</h1>
+      <h1>Detail Nasabah</h1>
     </div><!-- End Page Title -->
 
     <section class="section">
@@ -18,30 +18,58 @@
               <form>
                 <div class="row mb-3">
                   <div class="col-lg-12">
-                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Text</label>
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Nasabah ID</label>
                   </div>
                   <div class="w-auto font-normal font-sans">
-                    {{ $nasabah->LoanId }}
+                    {{ $nasabah->NasabahId }}
                   </div>
                 </div>
 
                 <div class="row mb-3">
                   <div class="col-lg-12">
-                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Text</label>
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Loan ID</label>
                   </div>
                   <div class="w-auto font-normal font-sans">
                     {{ $nasabah->LoanId }}
+                  </div>
+                </div>
+                
+                <div class="row mb-3">
+                  <div class="col-lg-12">
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Status Penyesuaian</label>
+                  </div>
+                  <div class="w-auto font-normal font-sans">
+                    {{ $nasabah->Status }}
+                  </div>
+                </div>
+                
+                <div class="row mb-3">
+                  <div class="col-lg-12">
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Status Eksekusi TIF</label>
+                  </div>
+                  <div class="w-auto font-normal font-sans">
+                    {{ $nasabah->StatusEksekusiTIF }}
                   </div>
                 </div>
 
                 <div class="row mb-3">
                   <div class="col-lg-12">
-                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Text</label>
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Starting Date GP</label>
                   </div>
                   <div class="w-auto font-normal font-sans">
-                    {{ $nasabah->LoanId }}
+                    {{ $nasabah->StartingDateGP }}
                   </div>
                 </div>
+                
+                <div class="row mb-3">
+                  <div class="col-lg-12">
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">End Date GP</label>
+                  </div>
+                  <div class="w-auto font-normal font-sans">
+                    {{ $nasabah->EndDateGP }}
+                  </div>
+                </div>
+                
               </form><!-- End General Form Elements -->
 
             </div>
@@ -52,16 +80,70 @@
 
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title"></h5>
+              {{-- <h5 class="card-title"></h5> --}}
 
               <!-- Advanced Form Elements -->
               <form>
                 <div class="row mb-3">
                   <div class="col-lg-12">
-                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Nasabah ID</label>
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Nama FIle</label>
                   </div>
                   <div class="w-auto font-normal font-sans">
-                    {{ $nasabah->NasabahId }}
+                    {{ $nasabah->NamaFile }}
+                  </div>
+                </div>
+                
+                <div class="row mb-3">
+                  <div class="col-lg-12">
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Siklus</label>
+                  </div>
+                  <div class="w-auto font-normal font-sans">
+                    {{ $nasabah->Siklus }}
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <div class="col-lg-12">
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Tanggal Pencairan</label>
+                  </div>
+                  <div class="w-auto font-normal font-sans">
+                    {{ $nasabah->TanggalPencairan }}
+                  </div>
+                </div>
+                
+                <div class="row mb-3">
+                  <div class="col-lg-12">
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Tanggal Pencairan Value</label>
+                  </div>
+                  <div class="w-auto font-normal font-sans">
+                    {{ $nasabah->TanggalPencairanValue }}
+                  </div>
+                </div>
+                
+                <div class="row mb-3">
+                  <div class="col-lg-12">
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Date Eksekusi TIF</label>
+                  </div>
+                  <div class="w-auto font-normal font-sans">
+                    {{ $nasabah->DateEksekusiTIF }}
+                  </div>
+                </div>
+                
+                <div class="row mb-3">
+                  <div class="col-lg-12">
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">Starting Date GP (Penyesuaian)</label>
+                  </div>
+                  <div class="w-auto font-normal font-sans">
+                    {{ $nasabah->StartingDateGP_Penyesuaian }}
+                  </div>
+                </div>
+                
+                <div class="row mb-3">
+                  <div class="col-lg-12">
+                    <label for="inputText" class="col-sm-2 col-form-label w-auto font-extralight text-sm italic">End Date GP (Penyesuaian)</label>
+                  </div>
+                  <div class="w-auto font-normal font-sans">
+                    {{ $nasabah->EndDateGP_Penyesuaian }}
                   </div>
                 </div>
 
